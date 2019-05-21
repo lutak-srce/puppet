3a1c37f11ef9459918bad6c0806e75dd5f8eca2d
Jakov Sosic <jsosic@gmail.com>
2019-05-21 11:51:58 +0200

    Don't warn about environment master

--- files/puppet_env.sh
+++ files/puppet_env.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+# Print puppet environment if not production or master
 PUPPET_ENV="$(grep 'environment.*=' /etc/puppetlabs/puppet/puppet.conf 2>/dev/null | cut -d= -f 2)"
-[[ -n $PUPPET_ENV ]] && [[ $PUPPET_ENV != " production" ]] && echo "Puppet environment:$PUPPET_ENV"
+[[ -n $PUPPET_ENV ]] && [[ $PUPPET_ENV != " production" ]] && [[ $PUPPET_ENV != " master" ]] && echo "Puppet environment:$PUPPET_ENV"
